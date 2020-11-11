eaecd733b6e5497fb49517bf5711ebe39ef80ddc
WarGrey Gyoudmon Ju <juzhenliang@gmail.com>
2020-11-11 17:26:15 +0800

    add namespace.rkt

---
+++ sgml/digitama/namespace.rkt
@@ -0,0 +1,20 @@
+#lang typed/racket/base
+
+(provide (all-defined-out))
+
+(require racket/string)
+(require racket/symbol)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(define xml-qname-has-namespace? : (-> Symbol Boolean)
+  (lambda [qname]
+    (string-contains? (symbol->immutable-string qname) ":")))
+
+(define xml-qname-split : (-> Symbol (Values Symbol Symbol))
+  (lambda [qname]
+    (define sqname : String (symbol->immutable-string qname))
+    
+    (cond [(not (string-contains? sqname ":")) (values qname qname)]
+          [else (let ([parts : (Listof String) (string-split sqname ":")])
+                  (cond [(or (null? parts) (null? (cdr parts)) (pair? (cddr parts))) (values qname qname)]
+                        [else (values (string->symbol (car parts)) (string->symbol (cadr parts)))]))])))
