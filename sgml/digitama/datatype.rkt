9dcb32f2a282751e108308628506c3eb8c93e0ab
WarGrey Gyoudmon Ju <juzhenliang@gmail.com>
2022-08-14 20:05:00 +0800

    refactor color notation

--- sgml/digitama/datatype.rkt
+++ sgml/digitama/datatype.rkt
@@ -1,11 +1,13 @@
 #lang typed/racket/base
 
 (provide (all-defined-out))
+(provide (rename-out [xml-attribute-value*+>integer xml-attribute-value*->natural]))
 
 (require racket/string)
 
 (require digimon/symbol)
 (require digimon/dimension)
+(require digimon/number)
 
 (require "grammar.rkt")
 (require "digicore.rkt")
@@ -25,6 +27,7 @@
     (cond [(string? v) v]
           [(symbol? v) (symbol->immutable-string v)]
           [(number? v) (number->string v)]
+          [(list? v) (string-join (map xml-attribute-datum->value v))]
           [else (~a v)])))
 
 (define xml-attribute-dimension->value : (-> (Pairof Real Symbol) String)
@@ -39,11 +42,54 @@
           [(xml:name? v) (symbol->immutable-string (xml:name-datum v))]
           [else (symbol-join (map xml:name-datum v))])))
 
+(define xml-attribute-value*->string-list : (->* (XML-Element-Attribute-Value*) ((U String Regexp)) (Listof String))
+  (lambda [v [sep #px"\\s+"]]
+    (cond [(xml:string? v) (string-split (xml:string-datum v) sep)]
+          [(list? v) (for/list : (Listof String) ([n (in-list v)]) (symbol->immutable-string (xml:name-datum n)))]
+          [(xml:name? v) (list (symbol->immutable-string (xml:name-datum v)))]
+          [else null])))
+
 (define xml-attribute-value*->keyword : (-> XML-Element-Attribute-Value* Keyword)
   (lambda [v]
     (cond [(xml:string? v) (string->keyword (xml:string-datum v))]
           [(xml:name? v) (symbol->keyword (xml:name-datum v))]
           [else (string->keyword (string-join (map symbol->immutable-string (map xml:name-datum v))))])))
+
+(define xml-attribute-value*->integer : (-> XML-Element-Attribute-Value* (Option Integer))
+  (lambda [v]
+    (cond [(xml:string? v) (string->integer (xml:string-datum v))]
+          [(xml:name? v) (string->integer (symbol->immutable-string (xml:name-datum v)))]
+          [else #false])))
+
+(define xml-attribute-value*+>integer : (-> XML-Element-Attribute-Value* (Option Natural))
+  (lambda [v]
+    (cond [(xml:string? v) (string->natural (xml:string-datum v))]
+          [(xml:name? v) (string->natural (symbol->immutable-string (xml:name-datum v)))]
+          [else #false])))
+
+(define xml-attribute-value*->flonum : (-> XML-Element-Attribute-Value* (Option Flonum))
+  (lambda [v]
+    (cond [(xml:string? v) (string->flonum (xml:string-datum v))]
+          [(xml:name? v) (string->flonum (symbol->immutable-string (xml:name-datum v)))]
+          [else #false])))
+
+(define xml-attribute-value*+>flonum : (-> XML-Element-Attribute-Value* (Option Nonnegative-Flonum))
+  (lambda [v]
+    (cond [(xml:string? v) (string+>flonum (xml:string-datum v))]
+          [(xml:name? v) (string+>flonum (symbol->immutable-string (xml:name-datum v)))]
+          [else #false])))
+
+(define xml-attribute-value*->number : (-> XML-Element-Attribute-Value* (Option Real))
+  (lambda [v]
+    (cond [(xml:string? v) (string->real (xml:string-datum v))]
+          [(xml:name? v) (string->real (symbol->immutable-string (xml:name-datum v)))]
+          [else #false])))
+
+(define xml-attribute-value*+>number : (-> XML-Element-Attribute-Value* (Option Nonnegative-Real))
+  (lambda [v]
+    (cond [(xml:string? v) (string+>real (xml:string-datum v))]
+          [(xml:name? v) (string+>real (symbol->immutable-string (xml:name-datum v)))]
+          [else #false])))
 
 (define xml-attribute-value*->symbol : (-> XML-Element-Attribute-Value* Symbol)
   (lambda [v]
@@ -80,7 +126,7 @@
 
     (cons n unit)))
 
-(define xml-attribute-value*->nonnegative-dimension : (->* (XML-Element-Attribute-Value*) (Symbol) XML-Nonnegative-Dimension)
+(define xml-attribute-value*+>dimension : (->* (XML-Element-Attribute-Value*) (Symbol) XML-Nonnegative-Dimension)
   (lambda [v [canonical-unit '||]]
     (define-values (n unit)
       (cond [(xml:string? v) (string->dimension (xml:string-datum v) canonical-unit #:ci? #false)]
@@ -98,7 +144,7 @@
 
     (cons n unit)))
 
-(define xml-attribute-value*->nonnegative-dimension/ci : (->* (XML-Element-Attribute-Value*) (Symbol) XML-Nonnegative-Dimension)
+(define xml-attribute-value*+>dimension/ci : (->* (XML-Element-Attribute-Value*) (Symbol) XML-Nonnegative-Dimension)
   (lambda [v [canonical-unit '||]]
     (define-values (n unit)
       (cond [(xml:string? v) (string->dimension (xml:string-datum v) canonical-unit #:ci? #true)]
@@ -107,3 +153,27 @@
 
     (cons (if (>= n 0.0) n +nan.0) unit)))
 
+(define xml-attribute-value*->type-list : (All (T) (->* (XML-Element-Attribute-Value* (-> XML-Element-Attribute-Value* (Option T)))
+                                                        ((U String Regexp))
+                                                        (Listof T)))
+  (lambda [v ->type-datum [sep #px"\\s*,\\s*"]]
+    (cond [(xml:string? v)
+           (let parse ([vs : (Listof String) (string-split (xml:string-datum v) sep)]
+                       [ts : (Listof T) null])
+             (cond [(null? vs) (reverse ts)]
+                   [else (let*-values ([(self rest) (values (car vs) (cdr vs))]
+                                       [(datum) (->type-datum (syn-remake-token v xml:string self))])
+                           (cond [(not datum) (parse rest ts)]
+                                 [else (parse rest (cons datum ts))]))]))]
+          [(list? v)
+           (let parse ([vs : (Listof XML:Name) v]
+                       [ts : (Listof T) null])
+             (cond [(null? vs) (reverse ts)]
+                   [else (let*-values ([(self rest) (values (car vs) (cdr vs))]
+                                       [(datum) (->type-datum (syn-remake-token self xml:string (symbol->immutable-string (xml:name-datum self))))])
+                           (cond [(not datum) (parse rest ts)]
+                                 [else (parse rest (cons datum ts))]))]))]
+          [(xml:name? v)
+           (let ([datum (->type-datum (syn-remake-token v xml:string (symbol->immutable-string (xml:name-datum v))))])
+             (if (not datum) null (list datum)))]
+          [else null])))
--- svg/digitama/grammar/datatype.rkt
+++ svg/digitama/grammar/datatype.rkt
@@ -20,13 +20,19 @@
   (syntax-parse stx #:literals [:]
     [(_ dim [canonical-unit unit ...]
         (~optional [suffix ...] #:defaults ([(suffix 1) null]))
-        (~optional (~seq #:env env ...) #:defaults ([(env 1) null])))
-     (with-syntax* ([svg-attribute-value*+>id (make-identifier #'dim "svg-attribute-value*+>~a")]
-                    [svg-attribute-value*->id (make-identifier #'dim "svg-attribute-value*->~a")]
-                    [svg-attribute-id->value (make-identifier #'dim "svg-attribute-~a->value")]
+        (~optional (~seq #:with [env ...]) #:defaults ([(env 1) null]))
+        (~optional (~seq #:alias [alias:id ...]) #:defaults ([(alias 1) null])))
+     (with-syntax* ([(svg:attr-value*+>dim svg:attr-value*+>alias ...) (map-identifiers #'(dim alias ...) "svg:attr-value*+>~a")]
+                    [(svg:attr-value*->dim svg:attr-value*->alias ...) (map-identifiers #'(dim alias ...) "svg:attr-value*->~a")]
+                    [(svg:attr-dim->value  svg:attr-alias->value ...)  (map-identifiers #'(dim alias ...) "svg:attr-~a->value")]
                     [svg:dim-check (make-identifier #'dim "svg:~a-check")])
        (syntax/loc stx
-         (begin (define dim-units : (Listof Symbol) '(canonical-unit unit ...))
+         (begin (provide (rename-out [svg:attr-value*+>dim svg:attr-value*+>alias]
+                                     [svg:attr-value*->dim svg:attr-value*->alias]
+                                     [svg:attr-dim->value  svg:attr-alias->value]))
+                ...
+
+                (define dim-units : (Listof Symbol) '(canonical-unit unit ...))
 
                 ;;; NOTE
                 ; SVG's units are subsets of CSS's, but client-code shouldn't be bothered by that,
@@ -39,19 +45,19 @@
                       (case u [(suffix) (void)] ... [else (unless (memq u dim-units) (make+exn:svg:unit value))]))
                     #true))
 
-                (define svg-attribute-value*+>id : (-> XML-Element-Attribute-Value* (Pairof Nonnegative-Flonum Symbol))
+                (define svg:attr-value*+>dim : (-> XML-Element-Attribute-Value* (Pairof Nonnegative-Flonum Symbol))
                   (lambda [value]
-                    (define dim (xml-attribute-value*->nonnegative-dimension value 'canonical-unit))
+                    (define dim (xml-attribute-value*+>dimension value 'canonical-unit))
                     (svg:dim-check value dim)
                     dim))
 
-                (define svg-attribute-value*->id : (-> XML-Element-Attribute-Value* (Pairof Flonum Symbol))
+                (define svg:attr-value*->dim : (-> XML-Element-Attribute-Value* (Pairof Flonum Symbol))
                   (lambda [value]
                     (define dim (xml-attribute-value*->dimension value 'canonical-unit))
                     (svg:dim-check value dim)
                     dim))
 
-                (define svg-attribute-id->value : (-> (Pairof Real Symbol) String)
+                (define svg:attr-dim->value : (-> (Pairof Real Symbol) String)
                   (lambda [datum]
                     (define-values (n u) (values (real->double-flonum (car datum)) (cdr datum)))
                     (xml-attribute-dimension->value
@@ -59,8 +65,57 @@
                            [(memq u dim-units) (cons n u)]
                            [else (cons (dim n u env ...) 'canonical-unit)])))))))]))
 
+(define-syntax (define-svg-lists stx)
+  (syntax-case stx [:]
+    [(_ [name : Type attr->datum] ...)
+     (with-syntax* ([(name->value ...) (map-identifiers #'(name ...) "svg:attr-value*->~a-list")])
+       (syntax/loc stx
+         (begin (define nmame->value : (-> XML-Element-Attribute-Value* (Listof Type))
+                  (lambda [v]
+                    (xml-attribute-value*->type-list attr->datum #px"\\s*,\\s*")))
+                ...)))]))
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (define-svg-dimension dim:angle     [deg rad grad])
 (define-svg-dimension dim:frequency [Hz kHz])
-(define-svg-dimension dim:length    [px em ex in cm mm pt pc] [%] #:env css-dimenv)
+(define-svg-dimension dim:length    [px em ex in cm mm pt pc] [%] #:with [css-dimenv] #:alias [coordinate])
 (define-svg-dimension dim:time      [s ms])
+
+(define svg:attr-value*->integer : (-> XML-Element-Attribute-Value* (Option Integer))
+  (lambda [v]
+    (define i (xml-attribute-value*->integer v))
+
+    (when (not i) (make+exn:svg:range v))
+    i))
+
+(define svg:attr-value*->number : (-> XML-Element-Attribute-Value* (Option Real))
+  (lambda [v]
+    (define i (xml-attribute-value*->number v))
+
+    (when (not i) (make+exn:svg:range v))
+    i))
+
+(define svg:attr-value*->name : (-> XML-Element-Attribute-Value* (Option Symbol))
+  (lambda [v]
+    (define s (xml-attribute-value*->string v))
+
+    (cond [(regexp-match? #px"[,()]|\\s" s) (make+exn:svg:range v) #false]
+          [else (string->symbol s)])))
+
+(define svg:attr-value*->number-pair : (-> XML-Element-Attribute-Value* (U (Pairof Real Real) Real False))
+  (lambda [v]
+    (define ns (xml-attribute-value*->type-list v svg:attr-value*->number))
+
+    (cond [(null? ns) (make+exn:svg:range v) #false]
+          [(null? (cdr ns)) (car ns)]
+          [(pair? (cddr ns)) (make+exn:svg:range v) (cons (car ns) (cadr ns))]
+          [else (cons (car ns) (cadr ns))])))
+
+(define svg:attr-value*->integer-pair : (-> XML-Element-Attribute-Value* (U (Pairof Integer Integer) Integer False))
+  (lambda [v]
+    (define ns (xml-attribute-value*->type-list v svg:attr-value*->integer))
+
+    (cond [(null? ns) (make+exn:svg:range v) #false]
+          [(null? (cdr ns)) (car ns)]
+          [(pair? (cddr ns)) (make+exn:svg:range v) (cons (car ns) (cadr ns))]
+          [else (cons (car ns) (cadr ns))])))
